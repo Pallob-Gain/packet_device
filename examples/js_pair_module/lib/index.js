70ed37cf265060bfc7ce062432159fbe079f4465
PALLOB KUMAR GAIN <pallobkgain@gmail.com>
2025-08-14 10:01:44 +0200

    Delete examples directory

--- examples/js_pair_module/lib/index.js
+++
@@ -1,5 +0,0 @@
-const {serialSystem,tcpSerialDevice,uartSerialDevice}=require('roboclaw');
-const PacketDevice = require('./PacketDevice.js');
-const Struct=require('./struct.V4.js');
-
-module.exports={PacketDevice,Struct,serialSystem,tcpSerialDevice,uartSerialDevice};
